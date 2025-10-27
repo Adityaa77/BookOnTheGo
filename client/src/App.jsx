8eff183032fd7715c8c6aa0babf9c392ed08fdd0
Aditya Singh <singhaditya0020@gmail.com>
2025-10-27 19:28:38 +0530

    making routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import {Home} from './pages/Home'
+import Movies from './pages/Movies'
+import MyBooking from './pages/MyBooking'
+import Favourite from './pages/Favourite'
+import MovieDetails from './pages/MovieDetails'
+import SeatLayout from './pages/SeatLayout'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/movies' element={<Movies/>} />
+        <Route path='/booking' element={<MyBooking/>} />
+        <Route path='/favourite' element={<Favourite/>} />
+        <Route path='/movies/:id' element={<MovieDetails/>} />
+        <Route path='/movies/:id/date' element={<SeatLayout/>} />
+      </Routes>
+    </>
   )
 }
 
