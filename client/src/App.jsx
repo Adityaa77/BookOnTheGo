12ff87975afb6e11191efef114b91432e95f2b18
Aditya Singh <singhaditya0020@gmail.com>
2025-10-27 19:54:18 +0530

    fixing the navbar and footer to all pages and initialising tailwind css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import {Home} from './pages/Home'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Movies from './pages/Movies'
 import MyBooking from './pages/MyBooking'
 import Favourite from './pages/Favourite'
 import MovieDetails from './pages/MovieDetails'
 import SeatLayout from './pages/SeatLayout'
-
+import Home from './pages/Home'
+import {Toaster} from 'react-hot-toast'
+import Footer from './components/Footer'
 const App = () => {
+
+  const ifAdminRoute =useLocation().pathname.startsWith('/admin')
+
+
   return (
     <>
-      <Navbar/>
+    <Toaster />
+      {!ifAdminRoute && <Navbar/>}{/*Use this to not show the navbar when admin login */}
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/movies' element={<Movies/>} />
@@ -20,6 +26,7 @@ const App = () => {
         <Route path='/movies/:id' element={<MovieDetails/>} />
         <Route path='/movies/:id/date' element={<SeatLayout/>} />
       </Routes>
+       {!ifAdminRoute && <Footer/>}{/*Use this to not show the Footer when admin  */}
     </>
   )
 }
