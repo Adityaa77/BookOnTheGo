3c76882ae396d879e8cbc2c779c082e562c4f9ae
Aditya Singh <singhaditya0020@gmail.com>
2025-10-29 02:13:53 +0530

    made blurcycle,movies section and updated navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -29,11 +29,11 @@ const Navbar = () => {
       <XIcon className='md:hidden absolute top-6 right-6 w-6 h-6 
       cursor-pointer' onClick={()=> setisOpen(!isOpen)}/>
       
-      <Link onClick={()=> {scrollTo(0,0),  setisOpen(false)}} to='/'>Home</Link>
-      <Link onClick={()=> {scrollTo(0,0),  setisOpen(false)}} to='/movies'>Movies</Link>
-      <Link onClick={()=> {scrollTo(0,0),  setisOpen(false)}} to='/'>Theaters</Link>
-      <Link onClick={()=> {scrollTo(0,0),  setisOpen(false)}} to='/'>Releases</Link>
-      <Link onClick={()=> {scrollTo(0,0),  setisOpen(false)}} to='/favourite'>Favourite</Link>
+      <Link onClick={()=> {scrollTo(0,0);  setisOpen(false)}} to='/'>Home</Link>
+      <Link onClick={()=> {scrollTo(0,0);  setisOpen(false)}} to='/movies'>Movies</Link>
+      <Link onClick={()=> {scrollTo(0,0);  setisOpen(false)}} to='/'>Theaters</Link>
+      <Link onClick={()=> {scrollTo(0,0);  setisOpen(false)}} to='/'>Releases</Link>
+      <Link onClick={()=> {scrollTo(0,0);  setisOpen(false)}} to='/favourite'>Favourite</Link>
       </div>
 
       <div className='flex items-center gap-8'>
