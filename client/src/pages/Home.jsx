af0db0cd05ab1020bb1922f694785272a2ea5810
Aditya Singh <singhaditya0020@gmail.com>
2025-10-31 01:37:22 +0530

    Added Trailer Section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HeroSection from '../components/HeroSection'
 import MoviesSection from '../components/MoviesSection'
+import TrailerSection from '../components/TrailerSection'
 
 const Home = () => {
   return (
     <>
       <HeroSection/>
       <MoviesSection/>
+      <TrailerSection/>
     </>
   )
 }
