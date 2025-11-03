eb158a8d7eb74a07185a9ee947498a6e890f9ba3
Aditya Singh <singhaditya0020@gmail.com>
2025-11-03 18:32:32 +0530

    Added elements used for admin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,12 @@ import SeatLayout from './pages/SeatLayout'
 import Home from './pages/Home'
 import {Toaster} from 'react-hot-toast'
 import Footer from './components/Footer'
+import Layout from './pages/admin/Layout'
+import Dashboard from './pages/admin/Dashboard'
+import AddShows from './pages/admin/AddShows'
+import ListShows from './pages/admin/ListShows'
+import ListBookings from './pages/admin/ListBookings'
+
 const App = () => {
 
   const ifAdminRoute =useLocation().pathname.startsWith('/admin')
@@ -25,6 +31,13 @@ const App = () => {
         <Route path='/favourite' element={<Favourite/>} />
         <Route path='/movies/:id' element={<MovieDetails/>} />
         <Route path='/movies/:id/:date' element={<SeatLayout/>} />
+
+        <Route path='/admin/*' element={<Layout/>}>
+         <Route index element={<Dashboard/>}/>
+         <Route path="add-shows" element={<AddShows/>}/>
+         <Route path="list-shows" element={<ListShows/>}/>
+         <Route path="list-booking" element={<ListBookings/>}/>
+        </Route>
       </Routes>
        {!ifAdminRoute && <Footer/>}{/*Use this to not show the Footer when admin  */}
     </>
---
+++ client/src/components/admin/AdminSIdebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminSIdebar = () => {
+  return (
+    <div>
+      <h1>Sidebar</h1>
+    </div>
+  )
+}
+
+export default AdminSIdebar
