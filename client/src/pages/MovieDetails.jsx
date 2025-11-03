6996d5eb7c453e9e5fc756682886d5afeba82592
Aditya Singh <singhaditya0020@gmail.com>
2025-11-03 13:08:10 +0530

    made the loading screen and added movie details

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,22 +1,26 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { dummyShowsData, dummyDateTimeData } from '../assets/assets'
 import BlurCircle from '../components/BlurCircle'
 import { Dice1, Heart, PlayCircleIcon, StarIcon } from 'lucide-react'
 import timeFormat from '../lib/timeformat'
 import DateSelect from '../components/DateSelect'
+import MovieCard from '../components/MovieCard'
+import Loading from '../components/Loading'
 
 const MovieDetails = () => {
+  const navigate=useNavigate();
   const {id} =useParams()
   const[show,setShow]=useState(null)
 
   const getShow= async()=>{
     const show =dummyShowsData.find(show=>show._id === id)
-
-    setShow({
+    if(show){
+     setShow({
       movie:show,
-      dateTime: dummyShowsData
+      dateTime: dummyDateTimeData
     })
+    }
   }
 
   useEffect(()=>{
@@ -73,17 +77,15 @@ const MovieDetails = () => {
       <p className='text-lg font-medium mt-20 mb-8'>You May ALso Like</p>
       <div className='flex flex-wrap max-sm:justify-center gap-8'>
         {dummyShowsData.slice(0,4).map((movie,index)=>(
-          <MovieDetails key={index} movie={movie}/>
+          <MovieCard key={index} movie={movie}/>
         ))}
       </div>
       <div className='flex justify-center mt-20'>
-        <button className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull
+        <button onClick={()=>{navigate('/movies'); scrollTo(0,0)}} className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull
         transition rounded-md font-medium cursor-pointer'>Show More</button>
       </div>
     </div>
-  ):<div>
-    Loading...
-  </div>
+  ):<Loading />
 }
 
 export default MovieDetails
