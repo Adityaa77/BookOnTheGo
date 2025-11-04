dfb2962be0e557ca3858f9758d7a11a97bc46766
Aditya Singh <singhaditya0020@gmail.com>
2025-11-04 19:29:32 +0530

    added inggest to the backend

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config' 
 import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { inngest, functions } from "./inngest/index.js"
 
 
 const app=express();
@@ -9,10 +12,14 @@ const port =3000;
 
 await connectDB();
 
+//Middlewares
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
 
 //Api Route
 app.get('/', (req,res)=>res.send('server is live!'))
+app.use('/api/inngest',serve({ client: inngest, functions }))
+
 
 app.listen(port, ()=>console.log(`Server listening at http://localhost:${port}`));
