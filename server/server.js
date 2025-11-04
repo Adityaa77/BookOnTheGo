74d208575fb5671a3b9bf5cb9ceebb7e81e865fb
Aditya Singh <singhaditya0020@gmail.com>
2025-11-04 16:52:25 +0530

    making basic server files and connecting it with mongo db

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config' 
+import connectDB from './configs/db.js';
+
+
+const app=express();
+const port =3000;
+
+await connectDB();
+
+app.use(express.json())
+app.use(cors())
+
+//Api Route
+app.get('/', (req,res)=>res.send('server is live!'))
+
+app.listen(port, ()=>console.log(`Server listening at http://localhost:${port}`));
